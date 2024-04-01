585ff361d61d94528a6f6ab1fb6e77062478e067
Andrew Alverson <aja@uark.edu>
2024-04-01 09:18:36 -0500

    split our code into functions

--- in-class-python-scripts-completed/multiply.py
+++ in-class-python-scripts-completed/multiply.py
@@ -1,17 +1,35 @@
 #! /usr/bin/env python3
 
-# ask for the size of the multiplication matrix
-size = int(input("Enter the size of your table: "))
+def get_input():
+    
+    # ask for the size of the multiplication matrix
+    size = int(input("Enter the size of your table: "))
 
-# calculate how many spaces we'll need for the output of each cell in the matrix
-cell_width = len(str((size * size))) + 1
+    return size
 
-# nested for loop to calculate i * j
-for i in range(1,size+1):
-    for j in range(1,size+1):
-        # nice formatted print statement for each cell
-        print("{:>{cell_width}}".format(i * j, cell_width = cell_width), end='')
-        # print("{:6d}".format(i * j), end='')
+def print_table(n):
 
-    # print a newline character at the end of each row
-    print()
+    # calculate how many spaces we'll need for the output of each cell in the matrix
+    cell_width = len(str((n * n))) + 1
+
+    # nested for loop to calculate i * j
+    for i in range(1,n+1):
+        for j in range(1,n+1):
+            # nice formatted print statement for each cell
+            print("{:>{cell_width}}".format(i * j, cell_width = cell_width), end='')
+            # print("{:6d}".format(i * j), end='')
+
+        # print a newline character at the end of each row
+        print()
+
+
+def main():
+    input_num = get_input()
+
+    print_table(input_num)
+
+
+# set the environment for this script
+# is it main, or is this a module being called by another script
+if __name__ == '__main__':
+    main()
