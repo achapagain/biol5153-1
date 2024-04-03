de435bbd10c2305ddccb12551ad79a3f42780c0c
Andrew Alverson <aja@uark.edu>
2024-04-03 09:41:35 -0500

    now parsing command line arguments with argparse

--- in-class-python-scripts-completed/fibonacci.py
+++ in-class-python-scripts-completed/fibonacci.py
@@ -1,10 +1,26 @@
 #! /usr/bin/env python3
 
+import argparse
 
-def get_input():
-    # get the input
-    number = input("Enter the number: ")
-    return number
+
+def get_args():
+    # create an ArgumentParser object
+    parser = argparse.ArgumentParser(description='This script returns the Fibonacci number at \
+        a specified location in the Fibonacci sequence')
+
+    # add positional arguments (these are the ones that are absolutely essential/required)
+    parser.add_argument("num", help="The Fibonacci number you wish to calculate", type=int)
+
+    # add optional arguments
+    # if 'store_true', this means assign 'True' if the argument is specified on the command
+    # line, so the default for 'store_true' is false
+    parser.add_argument("-v", "--verbose", help="Print verbose output or not", action='store_true')
+
+    # parse the actual arguments
+    args = parser.parse_args()
+
+    return args
+
 
 
 def fib(beyonce):
@@ -27,17 +43,27 @@ def print_output(number, fibonacci_number):
 
 
 def main():
-    # first get the input
-    number = get_input()
-
     # calculate the fibonacci number
-    fibonacci_number = fib(number)
+    fibonacci_number = fib(args.num)
 
     # print the output
-    print_output(number, fibonacci_number)
+    print_output(args.num, fibonacci_number)
+
+
+# get the command line arguments
+args = get_args()
 
 
 # set the environment for this script
 # is it main, or is this a module being called by another script
 if __name__ == '__main__':
     main()
+
+
+
+
+
+
+
+
+    
