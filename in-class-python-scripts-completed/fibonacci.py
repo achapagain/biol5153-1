469a879108d52df3d7de69f34a1d62e69980d87b
Andrew Alverson <aja@uark.edu>
2024-04-03 09:08:41 -0500

    split up code into functions

--- in-class-python-scripts-completed/fibonacci.py
+++ in-class-python-scripts-completed/fibonacci.py
@@ -1,17 +1,43 @@
 #! /usr/bin/env python3
 
-# get the input
-number = input("Enter the number: ")
 
-# initialize our variables for calculating the Fibonacci number at this position in the sequence
-a,b = 0,1
+def get_input():
+    # get the input
+    number = input("Enter the number: ")
+    return number
 
-# one way to calculate the Fibonacci number
-for i in range(int(number)):
-    a,b = b,a+b
 
-# store the result so it's obvious
-fibonacci_number = a
+def fib(beyonce):
+    # initialize our variables for calculating the Fibonacci number at this position in the sequence
+    a,b = 0,1
 
-# print the output
-print('The Fibonacci number for', number, 'is:', fibonacci_number)
+    # one way to calculate the Fibonacci number
+    for i in range(int(beyonce)):
+        a,b = b,a+b
+
+    # store the result so it's obvious
+    fibonacci_number = a
+
+    return fibonacci_number
+
+
+def print_output(number, fibonacci_number):
+    # print the output
+    print('The Fibonacci number for', number, 'is:', fibonacci_number)
+
+
+def main():
+    # first get the input
+    number = get_input()
+
+    # calculate the fibonacci number
+    fibonacci_number = fib(number)
+
+    # print the output
+    print_output(number, fibonacci_number)
+
+
+# set the environment for this script
+# is it main, or is this a module being called by another script
+if __name__ == '__main__':
+    main()
